b1e76adc967ec551ae44112f595239e92f8509bd
Ryan Alves de Oliveira <ryan.adoliveira23@gmail.com>
2024-11-09 16:34:52 -0300

    removido arquivos lixo

--- conecta-ai-web/src/app/(app)/novo-curriculo/page.tsx
+++ conecta-ai-web/src/app/(app)/novo-curriculo/page.tsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from '../../components/ui/button';
+import { Button } from '@/components/ui/button';
 
 interface ContactInformation {
   firstName: string;
