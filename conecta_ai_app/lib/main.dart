941683cda280e9efaadbf7611e57b1de1cdf27d9
Jhonatas Teixeira <jhonatas@statum.com.br>
2024-11-10 04:17:49 -0300

    progress

--- conecta_ai_app/lib/main.dart
+++ conecta_ai_app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:conecta_ai_app/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Carrega o .env com as variáveis de ambiente
-  await dotenv.load();
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) => runApp(const MyApp()));
